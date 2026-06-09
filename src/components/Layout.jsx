9fe353fbd9210bb4ed4b3ceb3d6688fa7c0b5773
sai-krishna26 <saikrishnavishwakarma@gmail.com>
2026-06-09 15:29:25 +0530

    removed unwanted files

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,10 +7,6 @@ const Layout = ({ children }) => {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: 'vertical',
-      gestureDirection: 'vertical',
-      smooth: true,
-      mouseMultiplier: 1,
       smoothTouch: false,
       touchMultiplier: 2,
       infinite: false,
